6cd1e901a1748e1bb9c5c6594d2746abc084bca6
Roman Moravčík <roman.moravcik@gmail.com>
2019-04-07 07:49:59 +0200

    Added second serial port for RPi

--- Marlin/src/pins/pins_RAMPS.h
+++ Marlin/src/pins/pins_RAMPS.h
@@ -80,13 +80,13 @@
 // Limit Switches
 //
 #define X_MIN_PIN           3
-#ifndef X_MAX_PIN
-  #define X_MAX_PIN         2
-#endif
-#define Y_MIN_PIN          14
-#define Y_MAX_PIN          15
-#define Z_MIN_PIN          18
-#define Z_MAX_PIN          19
+// #ifndef X_MAX_PIN
+//  #define X_MAX_PIN         2
+// #endif
+#define Y_MIN_PIN           2
+// #define Y_MAX_PIN          15
+#define Z_MIN_PIN          14
+// #define Z_MAX_PIN          19
 
 //
 // Z Probe (when not Z_MIN_PIN)
