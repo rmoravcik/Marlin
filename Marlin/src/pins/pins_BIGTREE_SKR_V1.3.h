38dee3224bf5b050caf56af895631443c58c5651
Roman Moravčík <roman.moravcik@gmail.com>
2019-05-08 13:02:21 +0200

    Modification for my wiring.

--- Marlin/src/pins/pins_BIGTREE_SKR_V1.3.h
+++ Marlin/src/pins/pins_BIGTREE_SKR_V1.3.h
@@ -160,20 +160,20 @@
 // Temperature Sensors
 //  3.3V max when defined as an analog input
 //
-#define TEMP_BED_PIN       0   // A0 (T0) - (67) - TEMP_BED_PIN
+#define TEMP_BED_PIN       2   // A2 (T2) - (69) - TEMP_1_PIN
 #define TEMP_0_PIN         1   // A1 (T1) - (68) - TEMP_0_PIN
-#define TEMP_1_PIN         2   // A2 (T2) - (69) - TEMP_1_PIN
+#define TEMP_1_PIN         0   // A0 (T0) - (67) - TEMP_BED_PIN
 
 //
 // Heaters / Fans
 //
 #define HEATER_0_PIN       P2_07
 #if HOTENDS == 1
-  #define FAN1_PIN         P2_04
+  #define FAN1_PIN         P2_03
 #else
-  #define HEATER_1_PIN     P2_04
+  #define HEATER_1_PIN     P2_03
 #endif
-#define FAN_PIN            P2_03
+#define FAN_PIN            P2_04
 #define HEATER_BED_PIN     P2_05
 
 /*
