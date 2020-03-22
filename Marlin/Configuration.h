910b3dcbb6cfb8c6a23d15f004656d6deb1e6e35
Roman Moravčík <roman.moravcik@gmail.com>
2020-03-22 12:11:23 +0100

    TMC2208 on extruder (#2)
    * Use TCM2208 on E0, decreased stepper currents.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -680,7 +680,7 @@
 #define Z2_DRIVER_TYPE TMC2208
 //#define Z3_DRIVER_TYPE A4988
 //#define Z4_DRIVER_TYPE A4988
-#define E0_DRIVER_TYPE DRV8825
+#define E0_DRIVER_TYPE TMC2208
 //#define E1_DRIVER_TYPE A4988
 //#define E2_DRIVER_TYPE A4988
 //#define E3_DRIVER_TYPE A4988
@@ -732,7 +732,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.583, 80.604, 400, 760.146 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.583, 80.604, 400, 372 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -1065,7 +1065,7 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR true
+#define INVERT_E0_DIR false
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
